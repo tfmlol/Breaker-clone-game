256a239818f6efb87d7ffe4655edf8376a98d545
tfmlol <tfm2lol@gmail.com>
2016-06-13 15:28:48 -0400

    v1.0 finished unmodified

--- js/script.js
+++ js/script.js
@@ -23,24 +23,37 @@ var ctx = $('#canvas')[0].getContext("2d");
 
 
 //BEGIN LIBRARY CODE
-var x = 150;
-var y = 150;
-var dx = 2;
-var dy = 4;
+var x = 25;
+var y = 250;
+var dx = 1.5;
+var dy = -4;
 var ctx;
 var WIDTH;
 var HEIGHT;
 var paddlex;
 var paddleh = 10;
 var paddlew = 75;
+var rightDown = false;
+var leftDown = false;
+var canvasMinX = 0;
+var canvasMaxX = 0;
 var intervalId = 0;
-
+var bricks;
+var NROWS = 5;
+var NCOLS = 5;
+var BRICKWIDTH;
+var BRICKHEIGHT = 15;
+var PADDING = 1;
 
 function init() {
   ctx = $('#canvas')[0].getContext("2d");
   WIDTH = $("#canvas").width();
   HEIGHT = $("#canvas").height();
-  return setInterval(draw, 10);
+  paddlex = WIDTH / 2;
+  BRICKWIDTH = (WIDTH/NCOLS) - 1;
+  canvasMinX = $("#canvas").offset().left;
+  canvasMaxX = canvasMinX + WIDTH;
+  intervalId = setInterval(draw, 10);
 }
 
 function circle(x,y,r) {
@@ -59,35 +72,14 @@ function rect(x,y,w,h) {
 
 function clear() {
   ctx.clearRect(0, 0, WIDTH, HEIGHT);
+  rect(0,0,WIDTH,HEIGHT);
 }
 
-function init() {
-  ctx = $('#canvas')[0].getContext("2d");
-  WIDTH = $("#canvas").width()
-  HEIGHT = $("#canvas").height()
-  paddlex = WIDTH / 2;
-  intervalId = setInterval(draw, 10);
-}
-
-//END LIBRARY CODE
-
-
-function init_paddle() {
-  paddlex = WIDTH / 2;
-  paddleh = 10;
-  paddlew = 75;
-}
-
-rightDown = false;
-leftDown = false;
-
-//set rightDown or leftDown if the right or left keys are down
 function onKeyDown(evt) {
   if (evt.keyCode == 39) rightDown = true;
   else if (evt.keyCode == 37) leftDown = true;
 }
 
-//and unset them when the right or left key is released
 function onKeyUp(evt) {
   if (evt.keyCode == 39) rightDown = false;
   else if (evt.keyCode == 37) leftDown = false;
@@ -96,26 +88,84 @@ function onKeyUp(evt) {
 $(document).keydown(onKeyDown);
 $(document).keyup(onKeyUp);
 
-//Make it bounce
-//Make the paddle
-function draw() {
-  clear();
-  circle(x, y, 10);
+function onMouseMove(evt) {
+  if (evt.pageX > canvasMinX && evt.pageX < canvasMaxX) {
+    paddlex = Math.max(evt.pageX - canvasMinX - (paddlew/2), 0);
+    paddlex = Math.min(WIDTH - paddlew, paddlex);
+  }
+}
 
-  //move the paddle if left or right is currently pressed
+$(document).mousemove(onMouseMove);
+
+function initbricks() {
+    bricks = new Array(NROWS);
+    for (i=0; i < NROWS; i++) {
+        bricks[i] = new Array(NCOLS);
+        for (j=0; j < NCOLS; j++) {
+            bricks[i][j] = 1;
+        }
+    }
+}
+
+function drawbricks() {
+  for (i=0; i < NROWS; i++) {
+    ctx.fillStyle = rowcolors[i];
+    for (j=0; j < NCOLS; j++) {
+      if (bricks[i][j] == 1) {
+        rect((j * (BRICKWIDTH + PADDING)) + PADDING,
+             (i * (BRICKHEIGHT + PADDING)) + PADDING,
+             BRICKWIDTH, BRICKHEIGHT);
+      }
+    }
+  }
+}
+
+//END LIBRARY CODE
+
+
+var ballr = 10;
+var rowcolors = ["#FF1C0A", "#FFFD0A", "#00A308", "#0008DB", "#EB0093"];
+var paddlecolor = "#FFFFFF";
+var ballcolor = "#FFFFFF";
+var backcolor = "#000000";
+
+function draw() {
+  ctx.fillStyle = backcolor;
+  clear();
+  ctx.fillStyle = ballcolor;
+  circle(x, y, ballr);
+
   if (rightDown) paddlex += 5;
   else if (leftDown) paddlex -= 5;
+  ctx.fillStyle = paddlecolor;
   rect(paddlex, HEIGHT-paddleh, paddlew, paddleh);
 
-  if (x + dx > WIDTH || x + dx < 0)
+  drawbricks();
+
+  //want to learn about real collision detection? go read
+  // http://www.metanetsoftware.com/technique/tutorialA.html
+  rowheight = BRICKHEIGHT + PADDING;
+  colwidth = BRICKWIDTH + PADDING;
+  row = Math.floor(y/rowheight);
+  col = Math.floor(x/colwidth);
+  //reverse the ball and mark the brick as broken
+  if (y < NROWS * rowheight && row >= 0 && col >= 0 && bricks[row][col] == 1) {
+    dy = -dy;
+    bricks[row][col] = 0;
+  }
+
+  if (x + dx + ballr > WIDTH || x + dx - ballr < 0)
     dx = -dx;
 
-  if (y + dy < 0)
+  if (y + dy - ballr < 0)
     dy = -dy;
-  else if (y + dy > HEIGHT) {
-    if (x > paddlex && x < paddlex + paddlew)
+  else if (y + dy + ballr > HEIGHT - paddleh) {
+    if (x > paddlex && x < paddlex + paddlew) {
+      //move the ball differently based on where it hit the paddle
+      dx = 8 * ((x-(paddlex+paddlew/2))/paddlew);
       dy = -dy;
-    else
+    }
+    else if (y + dy + ballr > HEIGHT)
       clearInterval(intervalId);
   }
 
@@ -124,3 +174,4 @@ function draw() {
 }
 
 init();
+initbricks();
